6cbef4098cb7e2a269f852d823c7d635a687eaec
accoforat <47168899+accoforat@users.noreply.github.com>
2020-01-14 07:12:35 +0300

    create help class for session

--- fixture/application.py
+++ fixture/application.py
@@ -1,19 +1,18 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-
+from fixture.sesssion import SessionHalper
 class Application:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.session = SessionHalper(self)
 
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+
 
     def group_creation(self, group):
         wd = self.wd
@@ -38,16 +37,7 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
 
     def destroy (self):
         self.wd.quit()
